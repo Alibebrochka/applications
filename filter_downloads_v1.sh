21037a67a4452fa1dfb8e7fab4d0faaf1f708e42
FemBoy <380952187125.tjoma@gmail.com>
2025-09-25 23:17:47 +0300

    filter_downloads_v1.sh + recursion

--- filter_downloads_v1.sh
+++ filter_downloads_v1.sh
@@ -1,38 +1,52 @@
 #!/bin/bash
-old="/home/alibebrochka/Downloads"
+
 books=$(find ~/ -type d -name 'books')
 music=$(find ~/ -type d -name 'music')
 videos=$(find ~/ -type d -name 'videos')
 appimages=$(find ~/ -type d -name 'appimages')
 rtorrent=$(find ~/.rtorrent -type d -name 'watch')
-for file in ~/Downloads/*; do
-    if [ -f "$file" ]; then
-        case "$file" in
-        *.pdf)
-            ~/applications/rep_elem.sh $old *.pdf $books
-            ;;
-        *.fb2)
-            ~/applications/rep_elem.sh $old *.fb2 $books
-            ;;
-        *.fb2.zip)
-            ~/applications/rep_elem.sh $old *.fb2.zip $books
-            ;;
-        *.mp3)
-            ~/applications/rep_elem.sh $old *.mp3 $music
-            ;;
-        *.mp4)
-            ~/applications/rep_elem.sh $old *.mp4 $videos
-            ;;
-        *.appimage)
-            ~/applications/rep_elem.sh $old *.appimage $appimages
-            ;;
-        *.AppImage)
-            ~/applications/rep_elem.sh $old *.AppImage $appimages
-            ;;
-        *.torrent)
-            ~/applications/rep_elem.sh $old *.torrent $rtorrent
-            ;;
-        *) ;;
-        esac
-    fi
-done
+
+process_directory() {
+    for file in "$1"/*; do
+        echo "$file"
+        if [ -d "$file" ]; then
+            process_directory "$file"
+        fi
+        if [ -f "$file" ]; then
+            case "$file" in
+            *.pdf)
+                ~/applications/rep_elem.sh "$1" *.pdf "$books"
+                ;;
+            *.fb2)
+                ~/applications/rep_elem.sh "$1" *.fb2 "$books"
+                ;;
+            *.fb2.zip)
+                ~/applications/rep_elem.sh "$1" *.fb2.zip "$books"
+                ;;
+            *.mp3)
+                ~/applications/rep_elem.sh "$1" *.mp3 "$music"
+                ;;
+            *.mp4)
+                ~/applications/rep_elem.sh "$1" *.mp4 "$videos"
+                ;;
+            *.appimage)
+                ~/applications/rep_elem.sh "$1" *.appimage "$appimages"
+                ;;
+            *.AppImage)
+                ~/applications/rep_elem.sh "$1" *.AppImage "$appimages"
+                ;;
+            *.torrent)
+                ~/applications/rep_elem.sh "$1" *.torrent "$rtorrent"
+                ;;
+                #*.rar)
+                #    unrar x $file
+                #    rm -r $file
+                #    ;;
+            *) ;;
+            esac
+        fi
+    done
+}
+
+old="/home/alibebrochka/Downloads"
+process_directory "$old"
